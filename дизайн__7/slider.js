4290833fa82f8276a539836fc543ae5caa56bea8
eusenov <145502985+eusenov@users.noreply.github.com>
2023-11-28 00:35:36 +0300

    Add files via upload

---
+++ дизайн__7/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.firstScreen-slider').slick({
+        dots: true
+    });
+
+    $('.fifthScreen-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
+      });
+});
